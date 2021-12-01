c2bebfcc06ff932576824604045d44a94a5c1cd3
opitt <23746293+opitt@users.noreply.github.com>
2021-12-01 11:05:08 +0100

    Solved Day1 2/2 tasks

--- day1/day1.py
+++ day1/day1.py
@@ -2,34 +2,33 @@
 from rich import print
 import os
 
-def main():
+def solve1(sonars):
+    ret=sum([(s2-s1)>0for s1,s2 in zip(sonars[:-1],sonars[1:])])
+    return ret
 
+def solve2(sonars):
+    sonars_3avg = [sum(sonars[i:i+3]) for i in range(len(sonars)-2)]
+    return solve1(sonars_3avg)
+
+def main():
     # READ INPUT FILE
     script_path = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(script_path, "input.txt"), encoding="utf-8") as input:
         lines = input.readlines()
+    lines = list(map(int, map(str.strip, lines)))
 
-    # HANDLE INPUT
-    lines = list(map(str.strip, lines))
-    lines = list(map(int, lines))
-    # lines = [1941, 1887, 1851, ...]
-
-    # CALCULATE PART 1
-    # example:
-    e1_2020 = [2020 - expense for expense in lines]
-    result = [d for d in e1_2020 if d in lines]
+    # PART 1
+    result=solve1(lines)
     print(
-        f"The entries ({result[0]},{result[1]}) sum up to 2020, and multiplied result in {result[0]*result[1]}"
+        f"The depth increases {result} times"
     )
+    # The depth increases 1688 times
 
-    pass
-    result = None
-    print(f"{result} ...")
-
-    # CALCULATE PART 2
-    pass
-    result = None
-    print(f"{result} ...")
-
+    # PART 2
+    result = solve2(lines)
+    print(
+        f"The depth increases {result} times"
+    )
+    # The depth increases 1728 times
 
 main()
