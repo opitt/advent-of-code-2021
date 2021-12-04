72b663832e84897ad2c56b6530288d973954a4f5
opitt <23746293+opitt@users.noreply.github.com>
2021-12-04 11:06:40 +0100

    refactored code - reuse functions for part 1 and 2

--- day4/day4.py
+++ day4/day4.py
@@ -4,93 +4,65 @@ from rich import print
 from more_itertools import flatten
 
 
+def mark_boards(boards, boards_marked, n):
+    for i, board in enumerate(boards):
+        if n in board:
+            boards_marked[i][board.index(n)] = n
+    pass
+
+def check_bingo(boards_marked, boards_with_bingo):
+    count = 0
+    for b, board in enumerate(boards_marked):
+        if b in boards_with_bingo:
+            continue
+        hori = any(
+            all(map(lambda n: n != None, board[row : row + 5]))
+            for row in range(0, 25, 5)
+        )
+        verti = any(
+            all(map(lambda n: n != None, board[col:25:5])) for col in range(5)
+        )
+        if verti or hori:
+            boards_with_bingo.append(b)
+            count += 1
+    return count>0
+
+def calc_score(board, marked, last_num):
+    # Start by finding the sum of all unmarked numbers on that board
+    res = sum(map(lambda bm: bm[0] if bm[1] == None else 0, zip(board, marked)))
+    # multiply it with the last number that made the bingo
+    return res * last_num
+
 def solve1(boards, draws):
-    def mark_boards(boards, boards_marked, n):
-        for i, board in enumerate(boards):
-            if n in board:
-                boards_marked[i][board.index(n)] = n
-        pass
-
-    def check_bingo(boards_marked):
-        bingo_board = None
-        for i, board in enumerate(boards_marked):
-            hori = any(
-                all(map(lambda n: n != None, board[row : row + 5]))
-                for row in range(0, 25, 5)
-            )
-            if hori:
-                bingo_board = i
-                break
-            verti = any(
-                all(map(lambda n: n != None, board[col:25:5])) for col in range(5)
-            )
-            if verti:
-                bingo_board = i
-                break
-        return bingo_board
-
-    def calc_score(board, marked, last_num):
-        # Start by finding the sum of all unmarked numbers on that board
-        res = sum(map(lambda bm: bm[0] if bm[1] == None else 0, zip(board, marked)))
-        # multiply it with the last number that made the bingo
-        return res * last_num
-
-    # find the first bingo board
+    # find the FIRST bingo board
     boards_marked = [[None] * 25 for _ in range(len(boards))]
+    boards_with_bingo=[]
     for num in draws:
         mark_boards(boards, boards_marked, num)
-        bingo_board = check_bingo(boards_marked)
-        if bingo_board != None:
+        if check_bingo(boards_marked,boards_with_bingo):
             last_num = num
             break
     # The score of the winning board can now be calculated.
-    return calc_score(boards[bingo_board], boards_marked[bingo_board], last_num)
-
+    return calc_score(boards[boards_with_bingo[-1]], boards_marked[boards_with_bingo[-1]], last_num)
 
 def solve2(boards, draws):
-    def mark_boards(boards, boards_marked, boards_with_bingo, n):
-        for b, board in enumerate(boards):
-            if n in board and b not in boards_with_bingo:
-                boards_marked[b][board.index(n)] = n
-        pass
-
-    def check_bingo(boards_marked, boards_with_bingo):
-        count=0
-        for b, board in enumerate(boards_marked):
-            if b in boards_with_bingo:
-                continue
-            hori = any(
-                all(map(lambda n: n != None, board[row : row + 5]))
-                for row in range(0, 25, 5)
-            )
-            verti = any(
-                all(map(lambda n: n != None, board[col:25:5])) 
-                for col in range(5)
-            )
-            if verti or hori:
-                boards_with_bingo.append(b)
-                count+=1
-                #return True
-        return count in (0,1)
-        #return False
-
-    def calc_score(board, marked, last_num):
-        # Start by finding the sum of all unmarked numbers on that board
-        res = sum(map(lambda bm: bm[0] if bm[1] == None else 0, zip(board, marked)))
-        # multiply it with the last number that made the bingo
-        return res * last_num
-
     # find the LAST bingo board
     boards_marked = [[None] * 25 for _ in range(len(boards))]
     boards_with_bingo = []
     for num in draws:
-        mark_boards(boards, boards_marked, boards_with_bingo, num)
+        mark_boards(boards, boards_marked, num)
         if check_bingo(boards_marked, boards_with_bingo):
+            # at least one bingo was found
             last_bingo_num = num
         if len(boards_with_bingo) == len(boards):
+            # all cards have a bingo now
             break
     # The score of the winning board can now be calculated.
-    return calc_score(boards[boards_with_bingo[-1]], boards_marked[boards_with_bingo[-1]], last_bingo_num)
+    return calc_score(
+        boards[boards_with_bingo[-1]],
+        boards_marked[boards_with_bingo[-1]],
+        last_bingo_num,
+    )
 
 
 def main():
@@ -119,7 +91,7 @@ def main():
     # PART 2
     result = solve2(boards, draws)
     print(f"The solution 2 is {result} ")
-    # answer:
+    # answer: 9576
 
 
 main()
