ae802bc3ab84813b0c6bb646e28e39dffc4a7c6b
opitt <23746293+opitt@users.noreply.github.com>
2021-12-05 08:06:45 +0100

    Reduce duplicate code

--- day5/day5.py
+++ day5/day5.py
@@ -1,8 +1,6 @@
 # https://adventofcode.com/2021/day/5
 import os
 from rich import print
-import math
-
 
 def mark_horiverti_line(vent_map, x1, y1, x2, y2):
     if not (x1 == x2 or y1 == y2):
@@ -11,33 +9,28 @@ def mark_horiverti_line(vent_map, x1, y1, x2, y2):
         for y in range(min(y1, y2), max(y1, y2) + 1):
             vent_map[y][x] += 1
 
-
-def solve1(lines_xy, x_max, y_max):
-    vent_map = [[0 for _ in range(x_max + 1)] for _ in range(y_max + 1)]
-    for x1, y1, x2, y2 in lines_xy:
-        mark_horiverti_line(vent_map, x1, y1, x2, y2)
-    overlaps = sum([1 for line in vent_map for cell in line if cell > 1])
-    return overlaps
-
-
 def mark_diaghoriverti_line(vent_map, x1, y1, x2, y2):
     if x1 == x2 or y1 == y2:
         mark_horiverti_line(vent_map, x1, y1, x2, y2)
         return
     sign=lambda n: 1 if n>=0 else -1
-    xsign = sign(x2 - x1)
-    ysign = sign(y2 - y1)
+    xsign, ysign = sign(x2 - x1), sign(y2 - y1)
+    # only 45 degrees given, i.e. x and y change by the same value
     for d in range(abs(x1 - x2) + 1):
-        vent_map[y1 + (d * ysign)][x1 + (d * xsign)] += 1
+        vent_map[y1 + ysign * d][x1 + xsign * d] += 1
 
+def calc_overlaps(vent_map):
+    return sum(1 for line in vent_map for cell in line if cell > 1)
 
-def solve2(lines_xy, x_max, y_max):
-    vent_map = [[0 for _ in range(x_max + 1)] for _ in range(y_max + 1)]
+def solve1(lines_xy, vent_map):
+    for x1, y1, x2, y2 in lines_xy:
+        mark_horiverti_line(vent_map, x1, y1, x2, y2)
+    return calc_overlaps(vent_map)
+
+def solve2(lines_xy, vent_map):
     for x1, y1, x2, y2 in lines_xy:
         mark_diaghoriverti_line(vent_map, x1, y1, x2, y2)
-    overlaps = sum([1 for line in vent_map for cell in line if cell > 1])
-    return overlaps
-
+    return calc_overlaps(vent_map)
 
 def main():
     # READ INPUT FILE
@@ -50,12 +43,14 @@ def main():
     y_max = max([max(line[1], line[3]) for line in lines])
 
     # PART 1
-    result = solve1(lines, x_max, y_max)
+    vent_map = [[0] * (x_max + 1) for _ in range(y_max + 1)]
+    result = solve1(lines, vent_map)
     print(f"The solution 1 is {result} ")
     # answer: 8111
 
     # PART 2
-    result = solve2(lines, x_max, y_max)
+    vent_map = [[0] * (x_max + 1) for _ in range(y_max + 1)]
+    result = solve2(lines, vent_map)
     print(f"The solution 2 is {result} ")
     # answer: 22088
 
