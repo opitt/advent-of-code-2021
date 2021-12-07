f523958344722f2d2b7884500a5742ecf5d9cb53
opitt <23746293+opitt@users.noreply.github.com>
2021-12-07 07:03:22 +0100

    Solved Day 7 part 2 logic error in part 1

--- day7/day7.py
+++ day7/day7.py
@@ -4,21 +4,40 @@ from rich import print
 from collections import Counter, defaultdict
 from copy import deepcopy
 
+
 def solve1(crabs):
-    #from_to_fuel={1:{2:2},2:{1:1}}
-    from_to_fuel={}
-    p_cnt=Counter(crabs)
-    pos=sorted(p_cnt.keys())
-    
+    # from_to_fuel={1:{2:2},2:{1:1}}
+    from_to_fuel = {}
+    p_cnt = Counter(crabs)
+    pos = sorted(p_cnt.keys())
     for p_from in pos:
-        from_to_fuel[p_from]={}
+        from_to_fuel[p_from] = {}
         for p_to in pos:
-            from_to_fuel[p_from][p_to]=abs(p_to-p_from)*p_cnt[p_from]
-    to_fuelneed={}
+            steps = abs(p_to - p_from)
+            from_to_fuel[p_from][p_to] = steps * p_cnt[p_from]
+    to_fuelneed = {}
     for p_to in pos:
         # test, if this is the best
-        to_fuelneed[p_to]=sum(from_to_fuel[p_from][p_to] for p_from in pos)
+        to_fuelneed[p_to] = sum(from_to_fuel[p_from][p_to] for p_from in pos)
     return min(to_fuelneed.values())
+
+
+def solve2(crabs):
+    # from_to_fuel={1:{2:2},2:{1:1}}
+    from_to_fuel = {}
+    p_cnt = Counter(crabs)
+    pos = sorted(p_cnt.keys())
+    for p_from in pos:
+        from_to_fuel[p_from] = {}
+        for p_to in range(min(pos), max(pos) + 1):
+            steps = abs(p_to - p_from)
+            from_to_fuel[p_from][p_to] = (steps * (steps + 1) // 2) * p_cnt[p_from]
+    to_fuelneed = {}
+    for p_to in range(min(pos), max(pos) + 1):
+        # test, if this is the best
+        to_fuelneed[p_to] = sum(from_to_fuel[p_from][p_to] for p_from in pos)
+    return min(to_fuelneed.values())
+
 
 def main():
     # READ INPUT FILE
@@ -31,7 +50,12 @@ def main():
     # PART 1
     result = solve1(crabs)
     print(f"The solution 1 is {result} ")
-    # answer: 
+    # answer: 323647
+
+    # PART 2
+    result = solve2(crabs)
+    print(f"The solution 2 is {result} ")
+    # answer:
 
 
 main()
