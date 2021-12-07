a13808017eb2277cd33ced77230c5fa68c8aedc0
opitt <23746293+opitt@users.noreply.github.com>
2021-12-07 07:07:15 +0100

    Fixed logic for part 1

--- day7/day7.py
+++ day7/day7.py
@@ -4,25 +4,7 @@ from rich import print
 from collections import Counter, defaultdict
 from copy import deepcopy
 
-
-def solve1(crabs):
-    # from_to_fuel={1:{2:2},2:{1:1}}
-    from_to_fuel = {}
-    p_cnt = Counter(crabs)
-    pos = sorted(p_cnt.keys())
-    for p_from in pos:
-        from_to_fuel[p_from] = {}
-        for p_to in pos:
-            steps = abs(p_to - p_from)
-            from_to_fuel[p_from][p_to] = steps * p_cnt[p_from]
-    to_fuelneed = {}
-    for p_to in pos:
-        # test, if this is the best
-        to_fuelneed[p_to] = sum(from_to_fuel[p_from][p_to] for p_from in pos)
-    return min(to_fuelneed.values())
-
-
-def solve2(crabs):
+def solve(crabs,double_burn):
     # from_to_fuel={1:{2:2},2:{1:1}}
     from_to_fuel = {}
     p_cnt = Counter(crabs)
@@ -31,7 +13,10 @@ def solve2(crabs):
         from_to_fuel[p_from] = {}
         for p_to in range(min(pos), max(pos) + 1):
             steps = abs(p_to - p_from)
-            from_to_fuel[p_from][p_to] = (steps * (steps + 1) // 2) * p_cnt[p_from]
+            if double_burn:
+                from_to_fuel[p_from][p_to] = (steps * (steps + 1) // 2) * p_cnt[p_from]
+            else:
+                from_to_fuel[p_from][p_to] = steps * p_cnt[p_from]
     to_fuelneed = {}
     for p_to in range(min(pos), max(pos) + 1):
         # test, if this is the best
@@ -48,14 +33,14 @@ def main():
     crabs = list(map(int, lines[0].strip().split(",")))
 
     # PART 1
-    result = solve1(crabs)
+    result = solve(crabs,False)
     print(f"The solution 1 is {result} ")
     # answer: 323647
 
     # PART 2
-    result = solve2(crabs)
+    result = solve(crabs,True)
     print(f"The solution 2 is {result} ")
-    # answer:
+    # answer: 87640209
 
 
 main()
