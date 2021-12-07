28b269f96675577fcd1916006dd23089581d8044
opitt <23746293+opitt@users.noreply.github.com>
2021-12-07 06:46:05 +0100

    Day 7 part 1

---
+++ day7/day7.py
@@ -0,0 +1,37 @@
+# https://adventofcode.com/2021/day/7
+import os
+from rich import print
+from collections import Counter, defaultdict
+from copy import deepcopy
+
+def solve1(crabs):
+    #from_to_fuel={1:{2:2},2:{1:1}}
+    from_to_fuel={}
+    p_cnt=Counter(crabs)
+    pos=sorted(p_cnt.keys())
+    
+    for p_from in pos:
+        from_to_fuel[p_from]={}
+        for p_to in pos:
+            from_to_fuel[p_from][p_to]=abs(p_to-p_from)*p_cnt[p_from]
+    to_fuelneed={}
+    for p_to in pos:
+        # test, if this is the best
+        to_fuelneed[p_to]=sum(from_to_fuel[p_from][p_to] for p_from in pos)
+    return min(to_fuelneed.values())
+
+def main():
+    # READ INPUT FILE
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(script_path, "input.txt"), encoding="utf-8") as input:
+        lines = input.readlines()
+    # 1,2,4,3,5 =>
+    crabs = list(map(int, lines[0].strip().split(",")))
+
+    # PART 1
+    result = solve1(crabs)
+    print(f"The solution 1 is {result} ")
+    # answer: 
+
+
+main()
