0ecdc8304a9670231e87c88049bc2a685ef19e64
opitt <23746293+opitt@users.noreply.github.com>
2021-12-09 20:40:49 +0100

    refactored code. more readable vars.

--- day9/day9.py
+++ day9/day9.py
@@ -2,58 +2,55 @@
 import os
 from rich import print
 from math import prod
-from copy import deepcopy
+
+MAXY = 0    # max vertical index (length of map-1)
+MAXX = 0    # max horizonal index (width of map-1)
+DIRECTIONS = ((0,-1),(0,1),(-1,0),(1,0)) # diff to current position to find adjacent positions
 
 
-def is_low(H, y, x):
-    height = H[y][x]
-    for dy, dx in DIRECTIONS:
-        if 0<=y+dy<=MAXY and 0<=x+dx<=MAXX:
-            height_neighbour = H[y+dy][x+dx]
-            if height >= height_neighbour:
-                return False
+def is_low(height_map, current_y, current_x):
+    height_current = height_map[current_y][current_x]
+    for dir_y, dir_x in DIRECTIONS:
+        next_y=current_y+dir_y
+        next_x=current_x+dir_x
+        if 0<=next_y<=MAXY and 0<=next_x<=MAXX:
+            height_next = height_map[next_y][next_x]
+            if height_current >= height_next:return False
     return True
 
 
-def solve(H):
+def solve(height_map):
     lows = []
-    for y in range(len(H)):
-        for x in range(len(H[0])):
-            if is_low(H, y, x):
-                lows.append(H[y][x])
+    for y in range(MAXY+1):
+        for x in range(MAXX+1):
+            if is_low(height_map, y, x):lows.append(height_map[y][x])
     return sum(lows) + len(lows)
 
 
-def explore_up(H, height, y, x):
-    H[y][x] = -1
-    explored_basins = 0
-    for dy, dx in DIRECTIONS:
-        ny=y+dy
-        nx=x+dx
-        if 0<=ny<=MAXY and 0<=nx<=MAXX:
-            height_neighbour = H[ny][nx]
-            if height_neighbour not in (9,-1): 
-                # if height_neighbour != 9 and height_neighbour > height:
-                explored_basins += 1+ explore_up(H, height_neighbour, ny, nx)
-    return explored_basins
+def explore_up(height_map_marked, height_current, current_y, current_x):
+    height_map_marked[current_y][current_x] = -1 # mark as visited and counted
+    basin_size = 0
+    for dir_y, dir_x in DIRECTIONS: #up, lef, right, down
+        next_y=current_y+dir_y
+        next_x=current_x+dir_x
+        if 0<=next_y<=MAXY and 0<=next_x<=MAXX:
+            height_next = height_map_marked[next_y][next_x]
+            if height_next not in (9,-1): 
+                basin_size += 1+ explore_up(height_map_marked, height_next, next_y, next_x)
+    return basin_size
 
 
 def solve2(height_map):
-    basins = []
-    for y in range(len(height_map)):
-        for x in range(len(height_map[0])):
-            if is_low(height_map, y, x):
-                height = height_map[y][x]
-                basin_size = 1 + explore_up(deepcopy(height_map), height, y, x)
-                # basin_size = 1 + explore_up(height_map, height, y, x)
-                basins.append(basin_size)
-    res = prod(sorted(basins, reverse=True)[:3])
+    basin_sizes = []
+    for y in range(MAXY+1):
+        for x in range(MAXX+1):
+            height = height_map[y][x]
+            if height not in (9,-1) and is_low(height_map, y, x):
+                basin_size = 1 + explore_up(height_map, height, y, x)
+                basin_sizes.append(basin_size)
+    # multiply the 3 biggest basin sizes
+    res = prod(sorted(basin_sizes, reverse=True)[:3])
     return res
-
-
-MAXY = 0
-MAXX = 0
-DIRECTIONS = ((0,-1),(0,1),(-1,0),(1,0))
 
 def main(input_name):
     # READ INPUT FILE
