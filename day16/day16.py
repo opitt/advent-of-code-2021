2f690db74514f719a58095c294056028e0278a61
opitt <23746293+opitt@users.noreply.github.com>
2021-12-19 21:45:03 +0100

    Part two produces wrong result

--- day16/day16.py
+++ day16/day16.py
@@ -1,89 +1,164 @@
 # https://adventofcode.com/2021/day/16
 import os
 from rich import print
+import queue as q
 
 
-def decode_literal_value(decoded, bits):
-    # Literal value packets encode a single binary number. To do this, the binary number is padded with leading zeroes until its length is a multiple of four bits, 
-    # and then it is broken into groups of four bits. 
-    # Each group is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit. These groups of five bits immediately follow the packet header. 
-    # For example, the hexadecimal string D2FE28 becomes: 110100101111111000101000
-    #                                                     VVVTTTAAAAABBBBBCCCCC
-    literal_value = ""
-    while True:
-        group=bits[:5]
-        bits=bits[5:]
-        literal_value+=group[1:]
-        if group[0]=="0":break
-    literal_value=int(literal_value,2) # 2021 for test
-    decoded.append({f"value":literal_value})
-    return decoded, bits
+def decode_stream(cur_op_q,cur_params_q, bits_remaining):
+    OPS={ 0:"add", 1:"mult", 2:"mi", 3:"ma", 5:"gt", 6:"lt", 7:"eq", 4:"val" }
 
-def decode_operator(decoded, bits, operator_id):
-    ops={ 0:"sum", 1:"*", 2:"min", 3:"max", 5:">", 6:"<", 7:"==" }
-    
-    # Every other type of packet (any packet with a type ID other than 4) represent an operator that performs some calculation on one or more sub-packets contained within. 
-    # Right now, the specific operations aren't important; focus on parsing the hierarchy of sub-packets.
-    # An operator packet contains one or more packets. 
-    # To indicate which subsequent binary data represents its sub-packets, 
-    #     an operator packet can use one of two modes indicated by the bit immediately after the packet header; this is called the length type ID:
-    #   - If the length type ID is 0, then the next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
-    #   - If the length type ID is 1, then the next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
-    p_length_type_id=bits[0]
-    bits=bits[1:]
-    if p_length_type_id=="0":
-        p_total_length_of_subpackets=int(bits[:15],2)
-        bits=bits[15:]
-        decoded.append({"op":f"{ops[operator_id]}","id":operator_id,"open":"("})
-        sub_bits=bits[:p_total_length_of_subpackets]
-        while len(sub_bits):
-            decoded, sub_bits = decode_nextpacket(decoded, sub_bits)
-        decoded.append({"id":operator_id,"close":")"})
-        bits=bits[p_total_length_of_subpackets:]        
+    while len(bits_remaining)!=bits_remaining.count("0"):
+        cur_op, cur_need=cur_op_q.get()
+        cur_params=cur_params_q.get()
+
+        if len(cur_params)<cur_need or cur_need<0:
+            # decode next block, need more parameters for the operator
+            cur_params_q.put(cur_params)
+            cur_op_q.put((cur_op,cur_need))
+        elif len(cur_params)==cur_need:
+            # merge the parameters with their operator
+            cur_params=cur_op + "("+ ",".join(cur_params) + ")"
+            # append operand with parameters as parameter - one level higher
+            p=cur_params_q.get()
+            p.append(cur_params)
+            cur_params_q.put(p)
+            continue
+            # decode next block ...
+        else:
+            raise ValueError
+            
+        p_version, p_type_id = int(bits_remaining[:3], 2), int(bits_remaining[3:6], 2)
+        bits_remaining = bits_remaining[6:]
+        match p_type_id:
+            case 0 | 1 | 2 | 3 | 5 | 6 | 7:
+                # operator packets
+                p_length_type_id=bits_remaining[0]
+                bits_remaining=bits_remaining[1:]
+                if p_length_type_id=="0":
+                    p_total_length_of_subpackets=int(bits_remaining[:15],2)
+                    bits_remaining=bits_remaining[15:]
+                    sub_packetbits=bits_remaining[:p_total_length_of_subpackets]
+
+                    cur_op_q.put((OPS[p_type_id],-1))
+                    cur_params_q.put([])
+
+                    decode_stream(cur_op_q,cur_params_q, sub_packetbits)
+                    bits_remaining=bits_remaining[p_total_length_of_subpackets:]        
+                else:
+                    p_number_of_subpackets=int(bits_remaining[:11],2)
+                    bits_remaining=bits_remaining[11:]
+
+                    cur_op_q.put((OPS[p_type_id],p_number_of_subpackets))
+                    cur_params_q.put([])        
+            case 4:
+                # Packets with type ID 4 represent a literal value. 
+                value = ""
+                while True:
+                    group=bits_remaining[:5]
+                    bits_remaining=bits_remaining[5:]
+                    value+=group[1:]
+                    if group[0]=="0":break
+                value=int(value,2)
+                # append value to current level's parameter list
+                cur_params=cur_params_q.get()
+                cur_params.append(str(value))
+                cur_params_q.put(cur_params)
+            case _:
+                raise ValueError
+
+    cur_op,cur_need=cur_op_q.get()
+    if cur_op!= "print":
+        cur_params=cur_params_q.get()
+        para_up=cur_params_q.get() # the next higher level
+        para_up.append(cur_op + "("+ ",".join(cur_params) + ")")
+        cur_params_q.put(para_up)
     else:
-        p_number_of_subpackets=int(bits[:11],2)
-        bits=bits[11:]
-        decoded.append({"op":f"{ops[operator_id]}","id":operator_id,"open":"(","args":p_number_of_subpackets})
-    return decoded, bits
-
-    # Finally, after the length type ID bit and the 15-bit or 11-bit field, the sub-packets appear.
+        cur_op_q.put((cur_op,cur_need))
+    return
     
-    # An operator packet contains one or more packets. 
-    # To indicate which subsequent binary data represents its sub-packets, 
-    # an operator packet can use one of two modes indicated by the bit immediately after the packet header; 
-    # this is called the length type ID:
-
-def decode_nextpacket(decoded, bits):
-    # Every packet begins with a standard header: the first three bits encode the packet version, and the next three bits encode the packet type ID.
-    # These two values are numbers; all numbers encoded in any packet are represented as binary with the most significant bit first. 
-    # For example, a version encoded as the binary sequence 100 represents the number 4.
-    p_version = int(bits[:3], 2)
-    decoded.append({"version":p_version})
-    p_type_id = int(bits[3:6], 2)
-    bits = bits[6:]
-    match p_type_id:
-        case 4:
-            # Packets with type ID 4 represent a literal value. 
-            decoded, bits = decode_literal_value(decoded,bits)
-        case _:
-            # operator packets
-            decoded, bits = decode_operator(decoded,bits,p_type_id)
-    return decoded, bits
-
-def decode_transmission(transmission):
-    decoded =[]
-    # The BITS transmission contains a single packet at its outermost layer which itself contains many other packets. 
-    # The hexadecimal representation of this packet might encode a few extra 0 bits at the end; these are not part of the transmission and should be ignored.
-    bits = "".join([f"{int(c,16):04b}" for c in transmission])
-    while len(bits)!=bits.count("0"):
-        decoded, bits = decode_nextpacket(decoded, bits)
-    return sum( d.get("version",0) for d in decoded), decoded
 
 def calculate(decoded):
+    import queue as q
     ops=[]
+    args=0
+    q_ops=q.LifoQueue()
+    q_arg=q.LifoQueue()
+    
     for d in decoded:
-        ops.extend([d.get("op",""), d.get("open",""), "#"+str(d.get("args","")) if d.get("args",0)>0 else "", d.get("close",""), str(d.get("value",""))])
-    print(" ".join(o for o in ops if len(o)))
+        match d:
+            case {"op":op, "open":_}:
+                ops.append(op+"(")
+                q_ops.put(ops[:])
+                q_arg.put(args)
+                ops,args=[],0
+
+            case {"op":op, "close":_}:
+                temp=q_ops.get()
+                temp.extend(ops)
+                temp.append("),")
+                ops=temp[:]
+                args=q_arg.get()
+                if args>0:
+                    args-=1
+                    if args==0:
+                        temp=q_ops.get()
+                        temp.extend(ops)
+                        temp.append("),")
+                        ops=temp[:]
+                        args=q_arg.get()
+
+            case {"op":op, "args":a}:
+                ops.append(op+"(")
+                q_ops.put(ops[:])
+                q_arg.put(args)
+                ops,args=[],a       
+            case {"value":v}:
+                ops.append(str(v)+",")
+                if args>0:
+                    args-=1
+                    if args==0:
+                        temp=q_ops.get()
+                        temp.extend(ops)
+                        temp.append("),")
+                        ops=temp[:]
+                        args=q_arg.get()
+
+        #ops.extend([d.get("op",""), d.get("open",""), "#"+str(d.get("args","")) if d.get("args",0)>0 else "", d.get("close",""), str(d.get("value",""))])
+    while not q_ops.empty():
+        temp=q_ops.get()
+        temp.extend(ops)
+        temp.append(")")
+        ops=temp[:]
+        args=q_arg.get()
+    pass
+    res = "".join(op for op in ops)
+    #res = res.replace(",)",")")
+    if res[-1]==",":
+        res = res[:-1]
+    print(res)
+    return
+
+def mult(*args):
+    import math 
+    return math.prod(args)
+
+def add(*args):
+    return sum(args)
+
+def ma(*args):
+    return max(args)
+
+def mi(*args):
+    return min(args)
+
+def gt(a,b):
+    return a>b
+
+def lt(a,b):
+    return a<b
+
+def eq(a,b):
+    return a==b
 
 def main(input_name,use_line=0):
     # READ INPUT FILE
@@ -92,21 +167,33 @@ def main(input_name,use_line=0):
         lines = input.readlines()
 
     transmission = [line.strip() for line in lines][use_line]
+    bits = "".join([f"{int(c,16):04b}" for c in transmission])
 
     # PART 1
-    result, decoded = decode_transmission(transmission)
-    print(f"The solution 1 is {result} ")
-    # answer: 986
-
-    # PART 2
-    result = calculate(decoded)
-    print(f"The solution 2 is {result} ")
-    # answer: 
+    cur_op_q=q.LifoQueue()      # queue: (operand, parameters expected)   e.g. ("mul",2) or ("ma":-1)
+    cur_params_q=q.LifoQueue()  # queue: [list of parameter strings]   e.g. ["1234","add(23,gt(1,2),4)"]
+    cur_op_q.put(("print",-1))  
+    cur_params_q.put([])
+ 
+    decode_stream(cur_op_q, cur_params_q, bits)
+    # now collapse the levels, nothin left to decode
+    while not cur_op_q.empty():
+        op, need = cur_op_q.get()
+        params = cur_params_q.get()
+        params = op + "(" + ",".join(params) + ")"
+        if not cur_op_q.empty():
+            # append parameter to above level
+            p = cur_params_q.get()
+            p.append(params)
+            cur_params_q.put(p)
+    
+    # result too low: 18217411328220
+    print(params)             
+    exec(params,globals())
 
 
 if __name__ == "__main__":
-    main("test.txt",4) # result: 12
-    main("test.txt",5) # result: 23
-    main("test.txt",6) # result: 31
+    #for i in range(7,15):
+    #    main("test.txt",i) 
     main("input.txt")  # result: 986 
     
