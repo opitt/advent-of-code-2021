5e53d7c8a9efcb8cb1b3e929c46e399f1ece558b
opitt <23746293+opitt@users.noreply.github.com>
2021-12-29 00:32:05 +0100

    Changed from list to dictionary. Part 1 solved.

--- day25/day25.py
+++ day25/day25.py
@@ -1,7 +1,6 @@
 # https://adventofcode.com/2021/day/25
 import os
 from rich import print
-from collections import defaultdict
 from copy import deepcopy
 
 SWARM_E = ">"
@@ -14,9 +13,9 @@ def print_swarms(swarms):
     grid = []
     for _ in range(SWARM_Y_MAX + 1):
         grid.append(list("." * (SWARM_X_MAX + 1)))
-    for y, x in swarms[SWARM_E]:
+    for y, x in swarms[SWARM_E].keys():
         grid[y][x] = SWARM_E
-    for y, x in swarms[SWARM_S]:
+    for y, x in swarms[SWARM_S].keys():
         grid[y][x] = SWARM_S
     for row in grid:
         print("".join(row))
@@ -24,33 +23,39 @@ def print_swarms(swarms):
 
 
 def move_one_step(swarms):
-    moves_east = moves_south = 0
-    new_pos = []
     # can east swarm move?
-    for y, x in swarms[SWARM_E]:
+    move_east = 0
+    new_pos = {}
+    for y, x in swarms[SWARM_E].keys():
+        y_new = y
         x_new = x + 1 if x < SWARM_X_MAX else 0
-        if (y, x_new) in swarms[SWARM_E] or (y, x_new) in swarms[SWARM_S]:
-            # can't move this cucumber
-            new_pos.append((y, x))
+        if swarms[SWARM_E].get((y_new, x_new), (-1, -1)) == (-1, -1) and swarms[
+            SWARM_S
+        ].get((y_new, x_new), (-1, -1)) == (-1, -1):
+            # nothing in the way, cucumber can move
+            move_east += 1
         else:
-            moves_east += 1
-            new_pos.append((y, x_new))
-    if moves_east:
-        swarms[SWARM_E] = new_pos[:]
-    # can south swarm move?
-    new_pos = []
-    for y, x in swarms[SWARM_S]:
-        y_new = y + 1 if y < SWARM_Y_MAX else 0
-        if (y_new, x) in swarms[SWARM_E] or (y_new, x) in swarms[SWARM_S]:
-            # can't move this cucumber
-            new_pos.append((y, x))
-        else:
-            moves_south += 1
-            new_pos.append((y_new, x))
-    if moves_south:
-        swarms[SWARM_S] = new_pos[:]
+            x_new = x
+        new_pos[(y_new, x_new)] = SWARM_E
+    if move_east:swarms[SWARM_E] = deepcopy(new_pos)
 
-    return moves_east + moves_south
+    # can south swarm move?
+    move_south = 0
+    new_pos = {}
+    for y, x in swarms[SWARM_S].keys():
+        y_new = y + 1 if y < SWARM_Y_MAX else 0
+        x_new = x
+        if swarms[SWARM_E].get((y_new, x_new), (-1, -1)) == (-1, -1) and swarms[
+            SWARM_S
+        ].get((y_new, x_new), (-1, -1)) == (-1, -1):
+            # nothing in the way, cucumber can move
+            move_south += 1
+        else:
+            y_new = y
+        new_pos[(y_new, x_new)] = SWARM_S
+    if move_south:swarms[SWARM_S] = deepcopy(new_pos)
+
+    return move_east + move_south
 
 
 def solve1(swarms):
@@ -71,12 +76,12 @@ def main(input_name):
     script_path = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(script_path, input_name), encoding="utf-8") as input:
         lines = input.readlines()
-    swarms = {SWARM_E: [], SWARM_S: []}
+    swarms = {SWARM_E: {}, SWARM_S: {}}
     for y, line in enumerate(lines):
         scan = line.strip()
         for x, cucumber in enumerate(scan):
             if cucumber != ".":
-                swarms[cucumber].append((y, x))
+                swarms[cucumber][(y, x)] = cucumber
     global SWARM_X_MAX, SWARM_Y_MAX
     SWARM_X_MAX = x
     SWARM_Y_MAX = y
