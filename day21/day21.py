87683f88d574f2bf1eb549f9673102e16b3e970e
opitt <23746293+opitt@users.noreply.github.com>
2021-12-29 12:59:23 +0100

    using constants, update while loop

--- day21/day21.py
+++ day21/day21.py
@@ -1,24 +1,24 @@
-# pos = [4, 8] # test
-pos = [1, 10] # start values
-dice = [6, 15] # start values
-score = [0, 0] # start values
 DICE_INC = 18
-winner = None
+MAX_POS = 10
+MAX_DICE = 100
+winner = looser = -1
+
+# pos = [4, 8] # test positions
+pos   = [1, 10] # start values
+dice  = [6, 15] # start values
+score = [0,  0] # start values
 rolls = 0
 
-while True:
+while winner < 0:
     for p in (0, 1):
-        rolls += 3
-
-        pos[p] = (pos[p] + dice[p]) % 10 or 10
-        dice[p] = (dice[p] + 18) % 100 or 10
+        rolls += 3 # three rolls
+        pos[p] = (pos[p] + dice[p]) % MAX_POS or MAX_POS
+        dice[p] = (dice[p] + DICE_INC) % 100 or MAX_DICE
 
         score[p] += pos[p]
         if score[p] >= 1000:
             winner = p
             looser = 0 if winner==1 else 1
             break
-    if winner != None:
-        break
 
 print(f"{rolls=}*{score[looser]=} = {rolls*score[looser]} ") # part 1: 428736
